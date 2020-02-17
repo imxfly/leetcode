cd785605f62d849e3e002caf85a63e43d17105c6
imxfly <stephenfxl@gmail.com>
2020-02-17 18:47:20 +0800

    Add algorithm <valid anagram>

---
+++ algorithms/golang/242-valid-anagram.go
@@ -0,0 +1,23 @@
+package main
+
+// https://leetcode-cn.com/problems/valid-anagram/
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+        return false
+    }
+	ht := make(map[rune]int, len(s))
+	// 考虑到 Unicode 字符集存在变长字节，所以需要分别对 s 和 t 进行循环遍历，不能合并
+	for _, ch := range s {
+		ht[ch]++
+	}
+	for _, ch := range t {
+		ht[ch]--
+	}
+	for _, num := range ht {
+		if num != 0 {
+			return false
+		}
+	}
+
+	return true
+}
