8fc713180a21f5a5c10676083a34c0aa97e6b481
imxfly <stephenfxl@gmail.com>
2020-02-24 10:04:23 +0800

    Add algorithm <valid parentheses>

---
+++ algorithms/golang/20-valid-parentheses.go
@@ -0,0 +1,44 @@
+package main
+
+type Stack struct {
+	top int
+	arr []rune
+}
+
+func (s *Stack) push(item rune) {
+	s.arr[s.top] = item
+	s.top++
+}
+
+func (s *Stack) pop() (rune, bool) {
+	if s.top == 0 {
+		return 0, false
+	}
+	s.top--
+	ch := s.arr[s.top]
+	return ch, true
+}
+
+// IsValid judge the validation of string
+func IsValid(s string) bool {
+	st := &Stack{
+		top: 0,
+		arr: make([]rune, len(s)),
+	}
+
+	for _, ch := range s {
+		if ch == 40 || ch == 91 || ch == 123 {
+			st.push(ch)
+		} else {
+			item, ok := st.pop()
+			if !ok {
+				return false
+			}
+			if item == 40 && ch != 41 || item == 91 && ch != 93 || item == 123 && ch != 125 {
+				return false
+			}
+		}
+	}
+
+	return st.top == 0
+}
