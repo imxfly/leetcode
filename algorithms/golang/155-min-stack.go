c1748c4aeafabfb5687818f0c9bfd8cd453b11dd
imxfly <stephenfxl@gmail.com>
2020-03-10 16:03:28 +0800

    Add algorithm <min stack>

---
+++ algorithms/golang/155-min-stack.go
@@ -0,0 +1,53 @@
+package main
+
+// MinStack stack
+type MinStack struct {
+	min  int   // 最小元素
+	data []int // 栈存储空间
+}
+
+// Constructor initialize your data structure here.
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+// Push push element to stack
+func (stack *MinStack) Push(x int) {
+	if len(stack.data) == 0 {
+		stack.min = x
+	} else if stack.min > x {
+		stack.min = x
+	}
+	stack.data = append(stack.data, x)
+}
+
+// Pop pop element from stack
+func (stack *MinStack) Pop() {
+	len := len(stack.data)
+	if len == 0 {
+		return
+	}
+
+	// 只有当出栈的元素是当前最小元素时，去重新选择最小元素
+	if stack.min == stack.data[len-1] {
+		min := stack.data[0]
+		for i := 0; i < len-1; i++ {
+			if min > stack.data[i] {
+				min = stack.data[i]
+			}
+		}
+		stack.min = min
+	}
+
+	stack.data = stack.data[:len-1]
+}
+
+// Top get the top element of stack
+func (stack *MinStack) Top() int {
+	return stack.data[len(stack.data)-1]
+}
+
+// GetMin get the minimum value of stack
+func (stack *MinStack) GetMin() int {
+	return stack.min
+}
--- algorithms/golang/27-remove-element.go
+++ algorithms/golang/27-remove-element.go
@@ -1,13 +1,13 @@
 package main
 
+// https://leetcode-cn.com/problems/remove-element/
 func removeElement(nums []int, val int) int {
 	i := 0
 	last := len(nums)
+
 	for i < last {
 		if nums[i] == val {
-			if nums[last-1] != val {
-				nums[i] = nums[last-1]
-			}
+			nums[i] = nums[last-1]
 			last--
 		} else {
 			i++
